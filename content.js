a3bf25fb02cf77512217cda420599b2375f01743
ErZicky <105796952+ErZicky@users.noreply.github.com>
2023-10-18 12:52:05 +0200

    Update to version 1.1

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+
+
 function createFaviconElement(domain, genericUrl) {
   const IconUrl = genericUrl.replace("%WEBSITE%", domain);
   const img = document.createElement("img");
@@ -6,31 +8,76 @@ function createFaviconElement(domain, genericUrl) {
   img.width = 18;
   img.style.marginTop = "4px";
   img.style.marginRight = "4px";
+  img.style.marginLeft = "2px";
   img.setAttribute("title", "Anonymous View");
   return img;
 }
 
-function addFaviconToContainer(container, favicon) {
-  const OriginalIcon = container.querySelector(".w-gl__anonymous-view-icon"); //the anonymousview icon that starpage put
-  OriginalIcon.insertAdjacentElement("beforebegin", favicon);
-  OriginalIcon.remove();
+function addFaviconToContainer(savedSettings, container, favicon) {
+  
+    const OriginalIcon = container.querySelector(".w-gl__anonymous-view-icon"); //the anonymousview icon that starpage put
+
+    console.log(savedSettings);
+    if(savedSettings != 1) //if != 1 replace the anonymousview icon
+    {
+     OriginalIcon.insertAdjacentElement("beforebegin", favicon);
+     OriginalIcon.remove();
+    }
+    else//else put it below the anonymousview icon
+    {
+       OriginalIcon.insertAdjacentElement("afterend", favicon);
+    }
+
 }
 
+
 function addFavicons() {
+
+
+
   const duckDuckGoFaviconUrl = "https://icons.duckduckgo.com/ip3/%WEBSITE%.ico";
   const resultContainers = document.querySelectorAll(".w-gl__result-url-container");
-  
+ 
   resultContainers.forEach((container) => {
     const url = container.querySelector(".w-gl__result-url").getAttribute("href");
     const website = new URL(url).hostname;
     
     const favicon = createFaviconElement(website, duckDuckGoFaviconUrl);
-    addFaviconToContainer(container, favicon);
+    RetrievePreference(container, favicon); //retrieve user settings
   });
 }
+
+
+function RetrievePreference(container, favicon)
+{
+    chrome.storage.local.get(['FavSettings'], function(result) {
+  if (chrome.runtime.lastError) 
+  {
+    console.error('Error retrieving data: ' + chrome.runtime.lastError);
+  } 
+  else 
+  {
+       addFaviconToContainer(result.FavSettings,container, favicon);
+  }
+});
+
+
+}
+
 
 function handleError(error) {
   console.error(error);
 }
 
+
+
+
+
 addFavicons();
+
+
+
+
+
+
+
---
+++ popup.js
@@ -0,0 +1,61 @@
+
+
+document.addEventListener("DOMContentLoaded", function () 
+{
+  const buttonA = document.getElementById("optionA"); //replace
+  const buttonB = document.getElementById("optionB"); //below
+
+
+  //load buttons status
+  function RetrievePreference(container, favicon)
+  {
+    chrome.storage.local.get(['FavSettings'], function(result) {
+      if (chrome.runtime.lastError) 
+      {
+        console.error('Error retrieving data: ' + chrome.runtime.lastError);
+      } 
+      else 
+      {
+          var status = result.FavSettings;
+          if(status != 1)
+          {
+            buttonA.classList.add("active");
+            buttonB.classList.remove("active");
+          }
+          else
+          {
+             buttonB.classList.add("active");
+             buttonA.classList.remove("active");
+          }
+      }
+
+    });
+
+
+ 
+
+  }
+
+  RetrievePreference();
+
+
+
+  // Buttons click events
+
+  buttonA.addEventListener("click", function () {
+       chrome.storage.local.set({ FavSettings: 0 });
+         // Aggiungi classe "active" al bottone A e rimuovi da B
+    buttonA.classList.add("active");
+    buttonB.classList.remove("active");
+   
+  });
+
+    buttonB.addEventListener("click", function () {
+       chrome.storage.local.set({ FavSettings: 1 });
+
+    buttonB.classList.add("active");
+    buttonA.classList.remove("active");
+  });
+
+
+});
